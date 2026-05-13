2f9a4c86e002ab0a017e54d0a2d9248b6351ab1d
phichaiyut deesungnern <89079183+phichaiyut@users.noreply.github.com>
2026-05-13 15:37:43 +0700

    Add files via upload

---
+++ libraries/MyRp_Pico2_Chea2_v2/example/MyRP_Pico2_3_Servo_V2_Kruink/MyRP_Pico2_3_Servo_V2_Kruink.ino
@@ -0,0 +1,45 @@
+#include <MyRP_Pico2.h>
+MyRP_Pico2 robot;
+#include <MyRp_Pico2_Chea_v2.h>
+//----------------------------------------------------------------------------->> ตั้งค่ามือจับ
+int servo_down = 55;    //-------------------->> ตั้งค่า มื่อจับลงขนานกับพื้น
+int servoL_open = 120;  //-------------------->> ตั้งค่า กางฝ่ามือด้านซ้าย
+int servoR_open = 120;  //-------------------->> ตั้งค่า กางฝ่ามือด้านขวา
+//----------------------------------------------------------------------------->> ตั้งค่ามือจับ
+//----------------------------------------------------------------------------->> ตั้งค่ามือจับ
+
+void setup() {
+  robot.begin();
+  set_motor();
+  Setting();
+  //_______________
+  S0_trim(0);
+  S1_trim(0);
+  S10_trim(0);
+  S28_trim(0);
+  //__________________
+  // arm_down_open();
+  arm_up_open();
+  // arm_down_open();
+  // arm_up_close();
+
+
+  sw();
+  delay(200);
+
+  // SetRobotAngle() ;
+  // SerialServoControl(); // 0 10 1 ตั้งค่าเซอร์โวผ่าน serial
+  Mission();
+
+  MotorStop();
+}
+
+void loop() {
+  // HoldAngle();
+  Serial.println(gyroZ());
+  sw();
+  delay(200);
+  // SetRobotAngle() ;
+  // SerialServoControl(); // 0 10 1 ตั้งค่าเซอร์โวผ่าน serial
+  Mission();
+}
